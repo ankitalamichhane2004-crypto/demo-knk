0ad7b1e719ae920bbfb782df43c8ded8bc6458ec
ankitalamichhane2004-crypto <ankitalamichhane2004@gmail.com>
2026-05-10 12:49:16 +0545

    edit frontend

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,4 @@ Route::get('/table',[FrontendController::class, 'table']);
 
 Route::get('/edit/{id}',[FrontendController::class, 'edit'])->name('editpage'); 
 
-Route::post('/update/{contact}',[FrontendController::class, 'update'])->name('update');
+Route::post('/update/{register}',[FrontendController::class, 'update'])->name('update');
